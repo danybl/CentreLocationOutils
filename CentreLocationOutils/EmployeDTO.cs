d7ae917fabcbe7c3a954a13e4a8b53a3e3df99fe
Dany Benoit Lafond <dany.benoit.lafond@gmail.com>
2014-11-12 09:35:19 -0500

    EmployeDTO initial

---
+++ CentreLocationOutils/EmployeDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CentreLocationOutils
+{
+    class EmployeDTO
+    {
+        public string IdEmploye{get;set;}
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Telephone{get;set;}
+        public string Email { get; set; }
+        public DateTime Daterecrutement { get; set; }
+        public string Poste { get; set; }
+    }
+}
