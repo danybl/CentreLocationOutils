abb6bd5a9085e05496df8e3295e288b0e0e92d5d
Dany Benoit Lafond <dany.benoit.lafond@gmail.com>
2014-11-13 13:33:13 -0500

    ajout dtos

---
+++ CentreLocationOutils/dto/Outil.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CentreLocationOutils.dto
+{
+    public class Outil
+    {
+        public string IdOutils { get; set; }
+        public string IdCategorie { get; set; }
+        public string Nom { get; set; }
+        public string NumSerie { get; set; }
+        public string DateAcquisition { get; set; }
+        public string PrixLocation { get; set; }
+        public string Description { get; set; }
+
+    }
+}
--- CentreLocationOutils/dto/ReservationDTO.cs
+++ CentreLocationOutils/dto/ReservationDTO.cs
@@ -5,15 +5,13 @@ using System.Text;
 
 namespace CentreLocationOutils.dto
 {
-    public class LocationDTO
+    public class ReservationDTO
     {
-        public string IdLocation { get; set; }
+        public string IdReservation { get; set; }
         public string IdClient { get; set; }
         public string IdEmploye { get; set; }
         public string IdOutils { get; set; }
-        public string Depot { get; set; }
-        public DateTime DateLocation { get; set; }
+        public DateTime DateReservation { get; set; }
         public DateTime DateLimite { get; set; }
-        public DateTime DateRetour { get; set; }
     }
 }
