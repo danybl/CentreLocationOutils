5063625559c314cb116ddc62fbae2850e088f419
David Gallego <davidgallego0125@gmail.com>
2014-11-18 10:41:08 -0500

    ajout du dossier lib

--- CentreLocationOutils/connexion.cs
+++ CentreLocationOutils/connexion.cs
@@ -1,17 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Drawing;
 using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
 namespace CentreLocationOutils
 {
-    public partial class LoginControl : UserControl
+    public partial class connexion : Form
     {
-        public LoginControl()
+        public connexion()
         {
             InitializeComponent();
         }
