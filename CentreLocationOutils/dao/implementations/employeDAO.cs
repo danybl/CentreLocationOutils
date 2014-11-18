f580d155437f900f7c55c61ce59efb200773bca2
Dany Benoit Lafond <dany.benoit.lafond@gmail.com>
2014-11-18 10:10:20 -0500

    a

--- CentreLocationOutils/dao/implementations/employeDAO.cs
+++ CentreLocationOutils/dao/implementations/employeDAO.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using CentreLocationOutils.dao.interfaces;
 
 namespace CentreLocationOutils.dao.implementations
 {
-    public class EmployeDAO
+    public class EmployeDAO :IEmployeDAO
     {
+
     }
 }
