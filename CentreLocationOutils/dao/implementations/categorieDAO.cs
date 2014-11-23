db01d13df53c1de8fd08f00006b274955c5b654b
Dany Benoit Lafond <dany.benoit.lafond@gmail.com>
2014-11-23 15:16:21 -0500

    Début

--- CentreLocationOutils/dao/implementations/categorieDAO.cs
+++ CentreLocationOutils/dao/implementations/categorieDAO.cs
@@ -2,10 +2,43 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using CentreLocationOutils.db;
+using CentreLocationOutils.exception.dto;
+using CentreLocationOutils.exception;
+using CentreLocationOutils.exception.dao;
+using CentreLocationOutils.dao.implementations;
+using System.Data;
+using CentreLocationOutils.dto;
+using System.Data.Common;
+using System.Data.OracleClient;
+using System.Runtime.Serialization;
+using CentreLocationOutils.dao.interfaces;
 
 namespace CentreLocationOutils.dao.implementations
 {
-    public class CategorieDAO
+    public class CategorieDAO : DAO, ICategorieDAO
     {
+        private static const string ADD_REQUEST = "INSERT INTO outil (idCategorie, nom, description) "
+      + "VALUES (:idOutil, :idCategorie, :nom, :numSerie, :dateAqcuisition, :prixLocation, :description)";
+
+        private static const string READ_REQUEST = "SELECT idOutil, idCategorie ,nom, numSerie, dateAqcuisition, prixLocation, description "
+           + "FROM outil "
+           + "WHERE idOutil = :idOutil";
+
+        private static const string UPDATE_REQUEST = "UPDATE outil "
+            + "SET idCategorie = :idCategorie, nom = :nom, numSerie = :numSerie, dateAcquisition = :dateAcquisition, prixLocation = :prixLocation, description = :description "
+            + "WHERE idOutil = :idOutil";
+
+        private static const string DELETE_REQUEST = "DELETE FROM outil "
+            + "WHERE idOutil = :idOutil";
+
+        private static const string GET_ALL_REQUEST = "SELECT idOutil, idCategorie ,nom, numSerie, dateAqcuisition, prixLocation, description "
+            + "FROM outil";
+
+        private static const string FIND_BY_NOM = "SELECT idOutil, idCategorie ,nom, numSerie, dateAqcuisition, prixLocation, description "
+            + "FROM outil "
+            + "where nom like :nom";
+
+        private static const string CREATE_PRIMARY_KEY = "SELECT SEQ_ID_OUTIL.NEXTVAL from DUAL";
     }
 }
--- CentreLocationOutils/dao/interfaces/ICategorieDAO.cs
+++ CentreLocationOutils/dao/interfaces/ICategorieDAO.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using CentreLocationOutils.db;
+using CentreLocationOutils.dto;
 
 namespace CentreLocationOutils.dao.interfaces
 {
-    public interface ICategorieDAO
+    public interface ICategorieDAO : IDAO
     {
+        ///<summary>Trouve les catégories à partir d'un nom. La liste est classée par ordre croissant sur <code>sortByPropertyName</code>. Si aucune catégorie
+        ///n'est trouvée, une {@link List} vide est retournée.</summary>>
+        /// <param name="connection">La connexion à utiliser</param>
+        ///<param name="nom">Le nom à trouver</param>
+        ///<param name="sortByPropertyName">The nom de la propriété à utiliser pour classer</param>
+        /// <returns> La liste des catégories correspondants à une liste vide sinon</returns>
+
+        List<CategorieDTO> findByNom(Connection connection,
+            String nom,
+            String sortByPropertyName);
     }
 }
