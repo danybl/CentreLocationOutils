a6f8e98559d989b3bfd6bd59fdfea41657ebe51d
CedricSoum <Cedric.Soumpholphakdy.1@collegeahuntsic.qc.ca>
2014-11-26 23:00:37 -0500

    ClientService

--- CentreLocationOutils/service/implementations/ClientService.cs
+++ CentreLocationOutils/service/implementations/ClientService.cs
@@ -1,11 +1,117 @@
-﻿using System;
+﻿using CentreLocationOutils.dao.interfaces;
+using CentreLocationOutils.db;
+using CentreLocationOutils.dto;
+using CentreLocationOutils.exception.dao;
+using CentreLocationOutils.exception.service;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace CentreLocationOutils.service.implementations
 {
-    class ClientService
+    public class ClientService : CentreLocationOutils.dao.implementations.Service, CentreLocationOutils.service.interfaces.IClientService
     {
+        private IClientDAO clientDAO;
+        private IReservationDAO reservationDAO;
+        
+        public ClientService(IClientDAO clientDAO,IReservationDAO reservationDAO) : base()
+        {
+          if(clientDAO == null) {
+              throw new InvalidDAOException("Le DAO de Client ne peut être null");
+        }
+        if(reservationDAO == null) {
+            throw new InvalidDAOException("Le DAO de réservation ne peut être null");
+        }
+        setClientDAO(clientDAO);
+        setReservationDAO(reservationDAO);
+        }
+
+        private IClientDAO getClientDAO()
+        {
+            return this.clientDAO;
+        }
+
+        private void setClientDAO(IClientDAO clientDAO)
+        {
+            this.clientDAO = clientDAO;
+        }
+
+        private IReservationDAO getReservationDAO()
+        {
+            return this.reservationDAO;
+        }
+
+        private void setReservationDAO(IReservationDAO reservationDAO)
+        {
+            this.reservationDAO = reservationDAO;
+        }
+
+        public void add(Connection connection,
+        ClientDTO clientDTO)
+        {
+        try {
+             getClientDAO().add(connection,clientDTO);
+        } catch (DAOException daoException){
+        throw new ServiceException(daoException.ToString());
+        }
+        }
+        
+        /*public ClientDTO get(Connection connection,String idClient){
+        try {
+            return (ClientDTO)getClientDAO().get(connection,idClient); 
+        }
+        catch (DAOException daoException)
+        {
+            throw new ServiceException(daoException.ToString());
+        }
+        } */
+
+        public void update(Connection connection,
+        ClientDTO clientDTO)
+        {
+            try
+            {
+                getClientDAO().update(connection, clientDTO);
+            }
+            catch (DAOException daoException)
+            {
+                throw new ServiceException(daoException.ToString());
+            }
+        }
+
+        public void delete(Connection connection,
+        ClientDTO clientDTO)
+        {
+            try
+            {
+                getClientDAO().delete(connection, clientDTO);
+            }
+            catch (DAOException daoException)
+            {
+                throw new ServiceException(daoException.ToString());
+            }
+        }
+      /*  public List<ClientDTO> getAll(Connection connection,String sortByPropertyName)
+        {
+            try
+            {
+                return (List<ClientDTO>)getClientDAO().getAll(connection,sortByPropertyName); 
+         }
+         catch (DAOException daoException) 
+            {
+                throw new ServiceException(daoException.ToString());
+            }
+        } */
+
+        public List<ClientDTO> findByNom(Connection connection,String nom,String sortByPropertyName){
+         try {
+         return getClientDAO().findByNom(connection,
+                nom,
+                sortByPropertyName);
+        } catch(DAOException daoException) {
+            throw new ServiceException(daoException.ToString());
+        }
+        }
     }
 }
--- CentreLocationOutils/service/interfaces/IClientService.cs
+++ CentreLocationOutils/service/interfaces/IClientService.cs
@@ -2,15 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using CentreLocationOutils.dao.interfaces;
-
+using CentreLocationOutils.service.interfaces;
+using CentreLocationOutils.dto;
+using CentreLocationOutils.db;
+using CentreLocationOutils.exception.service;
 
 namespace CentreLocationOutils.service.interfaces
 {
     /// <summary>
     /// 
     /// </summary>
-    public interface IClientService : IService
+    public interface IClientService : CentreLocationOutils.dao.interfaces.IService
     {
+        void add(Connection connection, ClientDTO clientDTO) {
+        throw new ServiceException;
+        }
+        ClientDTO get(Connection connection, String idClient){
+        throw new ServiceException;
+        }
+        void update(Connection connection, ClientDTO clientDTO){
+        throw new ServiceException;
+        }
+        void delete(Connection connection, ClientDTO clientDTO){
+        throw new ServiceException;
+        }
+        List<ClientDTO> findByNom(Connection connection,String nom,String SortByPropertyName)
+        {
+        throw new ServiceException;
+        }
     }
 }
