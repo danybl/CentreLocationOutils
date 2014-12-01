e294e4f7ed08466a8a9758aa89608241de2a77ea
Dany Benoit Lafond <dany.benoit.lafond@gmail.com>
2014-12-01 10:17:57 -0500

    méthodes métier

--- CentreLocationOutils/service/implementations/LocationService.cs
+++ CentreLocationOutils/service/implementations/LocationService.cs
@@ -15,13 +15,16 @@ namespace CentreLocationOutils.service.implementations
     public class LocationService : Service, ILocationService
     {
         private ILocationDAO locationDAO;
-       
+
+        private IReservationDAO reservationDAO;
+
 
         /// <summary>
         /// Crée le service de la table <code>location</code>.
         /// </summary>
         /// <param name="locationDAO">Le DAO de la table <code>location</code></param>
-        public LocationService(ILocationDAO locationDAO) : base()
+        public LocationService(ILocationDAO locationDAO, IReservationDAO reservationDAO)
+            : base()
         {
             if (locationDAO == null)
             {
@@ -51,13 +54,23 @@ namespace CentreLocationOutils.service.implementations
             this.locationDAO = locationDAO;
         }
 
+        private IReservationDAO getReservationDAO()
+        {
+            return this.reservationDAO;
+        }
+
+        private void setReservationDAO(IReservationDAO reservationDAO)
+        {
+            this.reservationDAO = reservationDAO;
+        }
+
         #endregion
 
         #region CRUD
 
 
         /// <inheritdoc />
-        public override void add(Connection connection, LocationDTO locationDTO)
+        public override void addLocation(Connection connection, LocationDTO locationDTO)
         {
             try
             {
@@ -70,7 +83,7 @@ namespace CentreLocationOutils.service.implementations
         }
 
         /// <inheritdoc />
-        public override LocationDTO get(Connection connection, string idLocation)
+        public override LocationDTO getLocation(Connection connection, string idLocation)
         {
             try
             {
@@ -83,7 +96,7 @@ namespace CentreLocationOutils.service.implementations
         }
 
         /// <inheritdoc />
-        public override void update(Connection connection, LocationDTO locationDTO)
+        public override void updateLocation(Connection connection, LocationDTO locationDTO)
         {
             try
             {
@@ -96,7 +109,7 @@ namespace CentreLocationOutils.service.implementations
         }
 
         /// <inheritdoc />
-        public override void delete(Connection connection, LocationDTO locationDTO)
+        public override void deleteLocation(Connection connection, LocationDTO locationDTO)
         {
             try
             {
@@ -123,7 +136,7 @@ namespace CentreLocationOutils.service.implementations
 
         #endregion
 
-        public void commencerPret(Connection connection, LocationDTO locationDTO)
+        public override void commencerLocation(Connection connection, LocationDTO locationDTO)
         {
             if (connection == null)
             {
@@ -133,7 +146,112 @@ namespace CentreLocationOutils.service.implementations
             {
                 throw new InvalidDTOException("La réservation ne peut être null");
             }
+
+            List<LocationDTO> locations = getLocationDAO().findByOutil(connection, locationDTO.OutilDTO.IdOutil, LocationDTO.ID_OUTIL_COLUMN_NAME);
+            foreach (LocationDTO uneLocationDTO in locations)
+            {
+                if (uneLocationDTO.DateRetour == null)
+                {
+                    throw new ExistingLoanException("Le livre "
+                        + uneLocationDTO.OutilDTO.Nom
+                        + " (ID d'outil : "
+                        + uneLocationDTO.OutilDTO.IdOutil
+                        + ") est présentement prêté à "
+                        + uneLocationDTO.ClientDTO.Nom
+                        + ", "
+                        + uneLocationDTO.ClientDTO.Prenom
+                        + "(ID de client : "
+                        + uneLocationDTO.ClientDTO.IdClient
+                        + ")");
+                }
+            }
+
+            if (locationDTO.ClientDTO.NbLocations.Equals(locationDTO.ClientDTO.LimiteLocations))
+            {
+                throw new InvalidLoanLimitException("Le client "
+                + locationDTO.ClientDTO.Nom
+                + ", "
+                + locationDTO.ClientDTO.Prenom
+                + " (ID de client : "
+                + locationDTO.ClientDTO.IdClient
+                + ") a atteint sa limite de locations ("
+                + locationDTO.ClientDTO.LimiteLocations
+                + " location(s) maximum)");
+            }
+
+            List<ReservationDTO> reservations = getReservationDAO().findByOutil(connection, locationDTO.OutilDTO.IdOutil, ReservationDTO.ID_OUTIL_COLUMN_NAME);
+            if (reservations.Count > 0)
+            {
+                ReservationDTO uneReservationDTO = reservations[0];
+                throw new ExistingReservationException("L'outil "
+                    + uneReservationDTO.OutilDTO.Nom
+                    + " (ID d'outil : "
+                    + uneReservationDTO.OutilDTO.IdOutil
+                    + ") est réservé pour "
+                    + uneReservationDTO.ClientDTO.Nom
+                    + ", "
+                    + uneReservationDTO.ClientDTO.Prenom
+                    + "(ID de client : "
+                    + uneReservationDTO.ClientDTO.IdClient
+                    + ")"
+                    );
+            }
+
+            locationDTO.ClientDTO.NbLocations = (int.Parse(locationDTO.ClientDTO.NbLocations + 1)).ToString();
+            locationDTO.DateLocation = System.DateTime.Today;
+            // locationDTO.DateRetour = null;
+            addLocation(connection, locationDTO);
         }
 
+        public override void renouvelerLocation(Connection connection, LocationDTO locationDTO)
+        {
+            if (connection == null)
+            {
+                throw new InvalidConnectionException("La connection ne peut être null");
+            }
+            if (locationDTO == null)
+            {
+                throw new InvalidDTOException("La location ne peut être null");
+            }
+            if (locationDTO.DateRetour != null)
+            {
+                throw new MissingLoanException("L'outil "
+                    + locationDTO.OutilDTO.Nom
+                    + " (ID d'outil : "
+                    + locationDTO.OutilDTO.IdOutil
+                    + ") est retourné"
+                    );
+            }
+            locationDTO.DateLocation = (System.DateTime.Now);
+            //locationDTO.DateRetour = null
+            updateLocation(connection, locationDTO);
+        }
+
+        public override void terminerLocation(Connection connection, LocationDTO locationDTO)
+        {
+            if (connection == null)
+            {
+                throw new InvalidConnectionException("La connection ne peut être null");
+            }
+            if (locationDTO == null)
+            {
+                throw new InvalidDTOException("La location ne peut être null");
+            }
+            if (locationDTO.DateRetour != null)
+            {
+                throw new MissingLoanException("L'outil "
+                    + locationDTO.OutilDTO.Nom
+                    + " (ID d'outil : "
+                    + locationDTO.OutilDTO.IdOutil
+                    + ") est retourné"
+                    );
+            }
+
+            locationDTO.ClientDTO.NbLocations = int.Parse(locationDTO.ClientDTO.NbLocations).ToString();
+            locationDTO.DateRetour = System.DateTime.Now;
+            updateLocation(connection, locationDTO);
+        }
+
+        //TODO findByDatePret() et findByDateRetour()
     }
 }
