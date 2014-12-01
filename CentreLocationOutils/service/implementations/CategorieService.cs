f062091279457b90da4d500dd6012061010a558b
franz <http://download.eclipse.org/egit/updates>
2014-12-01 10:10:38 -0500

    CategorieService et ICategorieService

--- CentreLocationOutils/service/implementations/CategorieService.cs
+++ CentreLocationOutils/service/implementations/CategorieService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using CentreLocationOutils.dao.implementations;
 using CentreLocationOutils.dao.interfaces;
 using CentreLocationOutils.exception.dao;
 using CentreLocationOutils.exception.dto;
@@ -15,136 +16,100 @@ namespace CentreLocationOutils.service.implementations
 {
     class CategorieService
     {
-        private IAdresseDAO adresseDAO;
+
         private ICategorieDAO categorieDAO;
-        private IClientDAO clientDAO;
-        private IEmployeDAO employeDAO;
-        private IFacturationDAO facturationDAO;
-        private ILocationDAO locationDAO;
-        private IOutilDAO outilDAO;
-        private IReservationDAO reservationDAO;
 
-        public CategorieService(IAdresseDAO adresseDAO, ICategorieDAO categorieDAO, IClientDAO clientDAO,
-            IEmployeDAO employeDAO, IFacturationDAO facturationDAO, ILocationDAO locationDAO, IOutilDAO outilDAO,
-            IReservationDAO reservationDAO) {
+        public CategorieService(ICategorieDAO categorieDAO) {
 
-                if (adresseDAO == null)
-                {
-                    throw new InvalidDAOException("Le DAO de adresse ne peut être null");
-                }
                 if (categorieDAO == null)
                 {
                     throw new InvalidDAOException("Le DAO de categorie ne peut être null");
                 }
-                if (clientDAO == null)
-                {
-                    throw new InvalidDAOException("Le DAO de client ne peut être null");
-                }
-                if (employeDAO == null)
-                {
-                    throw new InvalidDAOException("Le DAO de employe ne peut être null");
-                }
-                if (facturationDAO == null)
-                {
-                    throw new InvalidDAOException("Le DAO de facturation ne peut être null");
-                }
-                if (locationDAO == null)
-                {
-                    throw new InvalidDAOException("Le DAO de location ne peut être null");
-                }
-                if (outilDAO == null)
-                {
-                    throw new InvalidDAOException("Le DAO de outil ne peut être null");
-                }
-                if (reservationDAO == null)
-                {
-                    throw new InvalidDAOException("Le DAO de réservation ne peut être null");
-                }
-                setAdresseDAO(adresseDAO);
                 setCategorieDAO(categorieDAO);
-                setClientDAO(clientDAO);
-                setEmployeDAO(employeDAO);
-                setFacturationDAO(facturationDAO);
-                setLocationDAO(locationDAO);
-                setOutilDAO(outilDAO);
-                setReservationDAO(reservationDAO);
         }
-        private IAdresseDAO getAdresseDAO()
-        {
-            return this.adresseDAO;
-        }
+
+        //set et get
         private ICategorieDAO getCategorieDAO()
         {
             return this.categorieDAO;
-        }
-        private IClientDAO getClientDAO()
-        {
-            return this.clientDAO;
-        }
-        private IEmployeDAO getEmployeDAO()
-        {
-            return this.employeDAO;
-        }
-        private IFacturationDAO getFacturationDAO()
-        {
-            return this.facturationDAO;
-        }
-        private ILocationDAO getLocationDAO()
-        {
-            return this.locationDAO;
-        }
-        private IOutilDAO getOulilDAO()
-        {
-            return this.outilDAO;
-        }
-        private IReservationDAO getReservationDAO()
-        {
-            return this.reservationDAO;
-        }
-        private void setAdresseDAO(IAdresseDAO adresseDAO)
-        {
-            this.adresseDAO = adresseDAO;
         }
         private void setCategorieDAO(ICategorieDAO categorieDAO)
         {
             this.categorieDAO = categorieDAO;
         }
-        private void setClientDAO(IClientDAO clientDAO)
-        {
-            this.clientDAO = clientDAO;
+
+        //Ajout d'une catégorie
+        public void add(Connection connection, CategorieDTO categorieDTO) {
+            try {
+                getCategorieDAO().add(connection,
+                    categorieDTO);
+            }
+            catch (DbException dbException)
+            {
+                throw new DAOException(dbException);
+            }
         }
-        private void setEmployeDAO(IEmployeDAO employeDAO)
-        {
-            this.employeDAO = employeDAO;
+        //Lecture des catégories
+        public CategorieDTO get(Connection connection, String idCategorie) {
+            try {
+                return (CategorieDTO) getCategorieDAO().get(connection,
+                    idCategorie);
+            } catch(DbException dbException) {
+                throw new DAOException(dbException);
+            }
         }
-        private void setFacturationDAO(IFacturationDAO facturationDAO)
-        {
-            this.facturationDAO = facturationDAO;
+
+        //Mise à jour d'une catégorie
+        public void update(Connection connection,
+        CategorieDTO categorieDTO) 
+         {
+            try {
+                getCategorieDAO().update(connection,
+                    categorieDTO);
+            }
+            catch (DbException dbException)
+            {
+                throw new DAOException(dbException);
+            }
         }
-        private void setLocationDAO(ILocationDAO locationDAO)
-        {
-            this.locationDAO = locationDAO;
+        //Suppression d'une catégorie
+        public void delete(Connection connection,
+        CategorieDTO categorieDTO){
+            try {
+                getCategorieDAO().delete(connection,
+                    categorieDTO);
+            }
+            catch (DbException dbException)
+            {
+                throw new DAOException(dbException);
+            }
         }
-        private void setOutilDAO(IOutilDAO outilDAO)
-        {
-            this.outilDAO = outilDAO;
+        //Lecture de toutes les catégories
+        public List<CategorieDTO> getAll(Connection connection,
+        String sortByPropertyName){
+            try {
+                return (List<CategorieDTO>)getCategorieDAO().getAll(connection,
+                    sortByPropertyName);
+            }
+            catch (DbException dbException)
+            {
+                throw new DAOException(dbException);
+            }
         }
-        private void setReservationDAO(IReservationDAO reservationDAO)
-        {
-            this.reservationDAO = reservationDAO;
-        }
-    
-    //Ajout d'un livre
-    public void add(Connection connection,
-        CategorieDTO categorieDTO) {
+        //Trouver une catégorie par son nom
+        public List<CategorieDTO> findByNom(Connection connection,
+        String nom,
+        String sortByPropertyName){
         try {
-            getCategorieDAO().add(connection,
-                categorieDTO);
+            return getCategorieDAO().findByNom(connection,
+                nom,
+                sortByPropertyName);
         }
         catch (DbException dbException)
         {
             throw new DAOException(dbException);
         }
     }
+
     }
 }
