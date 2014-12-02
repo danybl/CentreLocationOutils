4b82a0e00a2e791301c6d90dba332ed85b18d35b
Dany Benoit Lafond <dany.benoit.lafond@gmail.com>
2014-12-02 15:43:47 -0500

    classes initiales

---
+++ CentreLcationOutils-front-end/util/CentreLocationOutilsCreateur.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CentreLcationOutils_front_end.util
+{
+    public class CentreLocationOutilsCreateur
+    {
+    }
+}
