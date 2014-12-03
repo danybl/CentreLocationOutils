0c161959eee13023e08e2323628ec684591d63f6
CedricSoum <Cedric.Soumpholphakdy.1@collegeahuntsic.qc.ca>
2014-12-03 11:14:49 -0500

    Les classes ClientFacade et IClientFacade

--- CentreLocationOutils/facade/interfaces/IClientFacade.cs
+++ CentreLocationOutils/facade/interfaces/IClientFacade.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using CentreLocationOutils.db;
+using CentreLocationOutils.dto;
 
 namespace CentreLocationOutils.facade.interfaces
 {
     public interface IClientFacade : IFacade
     {
+     public virtual List<ClientDTO> findByNom(Connection connection,String nom,String SortByPropertyName);
     }
 }
