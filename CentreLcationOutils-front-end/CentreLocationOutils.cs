ae039ca4b0f0b1b4101f85f8b5d6c449aa27c72d
Dany Benoit Lafond <dany.benoit.lafond@gmail.com>
2014-12-02 21:21:43 -0500

    Ajout de la référence pour utiliser le back-end

--- CentreLcationOutils-front-end/CentreLocationOutils.cs
+++ CentreLcationOutils-front-end/CentreLocationOutils.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using CentreLcationOutils_front_end.util;
+using CentreLocationOutils.dto;
 
 namespace CentreLcationOutils_front_end
 {
     public class CentreLocationOutils
     {
+        private CentreLocationOutilsCreateur centreLocationOutilsCreateur;
+
+        public void creerLocation(string[] args)
+        {
+            //ClientDTO
+        }
     }
 }
