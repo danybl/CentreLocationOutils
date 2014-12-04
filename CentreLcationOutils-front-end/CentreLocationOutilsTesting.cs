d6225304c67b7aee9a53f5c0f70d1e3b1f24a04f
Dany Benoit Lafond <dany.benoit.lafond@gmail.com>
2014-12-04 12:50:13 -0500

    for testing

--- CentreLcationOutils-front-end/CentreLocationOutilsTesting.cs
+++ CentreLcationOutils-front-end/CentreLocationOutilsTesting.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace CentreLocationOutils
+namespace CentreLcationOutils_front_end
 {
     class CentreLocationOutilsTesting
     {
