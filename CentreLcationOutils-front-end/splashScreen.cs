58e2418c4f9517829f1462cbcb2c7fa02650f5d2
David Gallego <davidgallego0125@gmail.com>
2014-12-04 13:02:11 -0500

    ajout splash au front end

---
+++ CentreLcationOutils-front-end/splashScreen.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CentreLocationOutils
+{
+    public partial class splashScreen : Form
+    {
+        Timer tmr;
+        public splashScreen()
+        {
+            InitializeComponent();
+        }
+            //Use timer class
+        
+        private void SplashScreen_Shown(object sender, EventArgs e)
+        {
+      tmr = new Timer();
+     //set time interval 3 sec
+     tmr.Interval = 3000;
+     //starts the timer
+     tmr.Start();
+     tmr.Tick += tmr_Tick;
+        }
+        void tmr_Tick(object sender, EventArgs e)
+        {
+         //after 3 sec stop the timer
+            progressBar1.Increment(5);
+            if (progressBar1.Value == progressBar1.Maximum)
+            {
+                tmr.Stop();
+                //display mainform
+                AccueilForm accueil = new AccueilForm();
+                accueil.Show();
+                //hide this form
+                this.Hide();
+
+            }
+        }
+        }
+}
