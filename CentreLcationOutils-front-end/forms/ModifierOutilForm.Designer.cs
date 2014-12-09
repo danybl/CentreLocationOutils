9aeb0687bed3fb54651d83f2ed60b9a7c71b6dec
David Gallego <davidgallego0125@gmail.com>
2014-12-09 12:51:38 -0500

    ajout des interfacebooks

---
+++ CentreLcationOutils-front-end/forms/ModifierOutilForm.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace CentreLocationOutils
+{
+    partial class ModifierOutilForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnModifierOutilAnnuler = new System.Windows.Forms.Button();
+            this.btnModifierOutilOk = new System.Windows.Forms.Button();
+            this.txtModifierOutilNbLocations = new System.Windows.Forms.TextBox();
+            this.txtModifierOutilDateInscription = new System.Windows.Forms.TextBox();
+            this.txtModifierOutilEmail = new System.Windows.Forms.TextBox();
+            this.txtModifierOutilTelephone = new System.Windows.Forms.TextBox();
+            this.txtModifierOutilPrenom = new System.Windows.Forms.TextBox();
+            this.txtModifierOutilNom = new System.Windows.Forms.TextBox();
+            this.txtModifierOutilIdOutil = new System.Windows.Forms.TextBox();
+            this.lblModifierOutilDescription = new System.Windows.Forms.Label();
+            this.lblModifierOutilPrixLocation = new System.Windows.Forms.Label();
+            this.lblModifierOutilDateAcquisition = new System.Windows.Forms.Label();
+            this.lblModifierOutilPrenom = new System.Windows.Forms.Label();
+            this.lblModifierOutilNom = new System.Windows.Forms.Label();
+            this.lblModifierOutilIdCategorie = new System.Windows.Forms.Label();
+            this.lblModifierOutilIdOutil = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnModifierOutilAnnuler
+            // 
+            this.btnModifierOutilAnnuler.Location = new System.Drawing.Point(153, 312);
+            this.btnModifierOutilAnnuler.Name = "btnModifierOutilAnnuler";
+            this.btnModifierOutilAnnuler.Size = new System.Drawing.Size(75, 23);
+            this.btnModifierOutilAnnuler.TabIndex = 65;
+            this.btnModifierOutilAnnuler.Text = "Annuler";
+            this.btnModifierOutilAnnuler.UseVisualStyleBackColor = true;
+            // 
+            // btnModifierOutilOk
+            // 
+            this.btnModifierOutilOk.Location = new System.Drawing.Point(38, 312);
+            this.btnModifierOutilOk.Name = "btnModifierOutilOk";
+            this.btnModifierOutilOk.Size = new System.Drawing.Size(75, 23);
+            this.btnModifierOutilOk.TabIndex = 64;
+            this.btnModifierOutilOk.Text = "OK";
+            this.btnModifierOutilOk.UseVisualStyleBackColor = true;
+            // 
+            // txtModifierOutilNbLocations
+            // 
+            this.txtModifierOutilNbLocations.Location = new System.Drawing.Point(143, 273);
+            this.txtModifierOutilNbLocations.Name = "txtModifierOutilNbLocations";
+            this.txtModifierOutilNbLocations.Size = new System.Drawing.Size(105, 20);
+            this.txtModifierOutilNbLocations.TabIndex = 63;
+            // 
+            // txtModifierOutilDateInscription
+            // 
+            this.txtModifierOutilDateInscription.Location = new System.Drawing.Point(143, 235);
+            this.txtModifierOutilDateInscription.Name = "txtModifierOutilDateInscription";
+            this.txtModifierOutilDateInscription.Size = new System.Drawing.Size(105, 20);
+            this.txtModifierOutilDateInscription.TabIndex = 62;
+            // 
+            // txtModifierOutilEmail
+            // 
+            this.txtModifierOutilEmail.Location = new System.Drawing.Point(143, 195);
+            this.txtModifierOutilEmail.Name = "txtModifierOutilEmail";
+            this.txtModifierOutilEmail.Size = new System.Drawing.Size(105, 20);
+            this.txtModifierOutilEmail.TabIndex = 61;
+            // 
+            // txtModifierOutilTelephone
+            // 
+            this.txtModifierOutilTelephone.Location = new System.Drawing.Point(143, 157);
+            this.txtModifierOutilTelephone.Name = "txtModifierOutilTelephone";
+            this.txtModifierOutilTelephone.Size = new System.Drawing.Size(105, 20);
+            this.txtModifierOutilTelephone.TabIndex = 60;
+            // 
+            // txtModifierOutilPrenom
+            // 
+            this.txtModifierOutilPrenom.Location = new System.Drawing.Point(143, 120);
+            this.txtModifierOutilPrenom.Name = "txtModifierOutilPrenom";
+            this.txtModifierOutilPrenom.Size = new System.Drawing.Size(105, 20);
+            this.txtModifierOutilPrenom.TabIndex = 59;
+            // 
+            // txtModifierOutilNom
+            // 
+            this.txtModifierOutilNom.Location = new System.Drawing.Point(143, 80);
+            this.txtModifierOutilNom.Name = "txtModifierOutilNom";
+            this.txtModifierOutilNom.Size = new System.Drawing.Size(105, 20);
+            this.txtModifierOutilNom.TabIndex = 58;
+            // 
+            // txtModifierOutilIdOutil
+            // 
+            this.txtModifierOutilIdOutil.Location = new System.Drawing.Point(143, 42);
+            this.txtModifierOutilIdOutil.Name = "txtModifierOutilIdOutil";
+            this.txtModifierOutilIdOutil.ReadOnly = true;
+            this.txtModifierOutilIdOutil.Size = new System.Drawing.Size(105, 20);
+            this.txtModifierOutilIdOutil.TabIndex = 57;
+            // 
+            // lblModifierOutilDescription
+            // 
+            this.lblModifierOutilDescription.AutoSize = true;
+            this.lblModifierOutilDescription.Location = new System.Drawing.Point(20, 276);
+            this.lblModifierOutilDescription.Name = "lblModifierOutilDescription";
+            this.lblModifierOutilDescription.Size = new System.Drawing.Size(60, 13);
+            this.lblModifierOutilDescription.TabIndex = 56;
+            this.lblModifierOutilDescription.Text = "Description";
+            // 
+            // lblModifierOutilPrixLocation
+            // 
+            this.lblModifierOutilPrixLocation.AutoSize = true;
+            this.lblModifierOutilPrixLocation.Location = new System.Drawing.Point(20, 238);
+            this.lblModifierOutilPrixLocation.Name = "lblModifierOutilPrixLocation";
+            this.lblModifierOutilPrixLocation.Size = new System.Drawing.Size(79, 13);
+            this.lblModifierOutilPrixLocation.TabIndex = 55;
+            this.lblModifierOutilPrixLocation.Text = "Prix de location";
+            // 
+            // lblModifierOutilDateAcquisition
+            // 
+            this.lblModifierOutilDateAcquisition.AutoSize = true;
+            this.lblModifierOutilDateAcquisition.Location = new System.Drawing.Point(20, 198);
+            this.lblModifierOutilDateAcquisition.Name = "lblModifierOutilDateAcquisition";
+            this.lblModifierOutilDateAcquisition.Size = new System.Drawing.Size(91, 13);
+            this.lblModifierOutilDateAcquisition.TabIndex = 54;
+            this.lblModifierOutilDateAcquisition.Text = "Date d\'acquisition";
+            // 
+            // lblModifierOutilPrenom
+            // 
+            this.lblModifierOutilPrenom.AutoSize = true;
+            this.lblModifierOutilPrenom.Location = new System.Drawing.Point(20, 160);
+            this.lblModifierOutilPrenom.Name = "lblModifierOutilPrenom";
+            this.lblModifierOutilPrenom.Size = new System.Drawing.Size(84, 13);
+            this.lblModifierOutilPrenom.TabIndex = 53;
+            this.lblModifierOutilPrenom.Text = "Numero de série";
+            // 
+            // lblModifierOutilNom
+            // 
+            this.lblModifierOutilNom.AutoSize = true;
+            this.lblModifierOutilNom.Location = new System.Drawing.Point(20, 123);
+            this.lblModifierOutilNom.Name = "lblModifierOutilNom";
+            this.lblModifierOutilNom.Size = new System.Drawing.Size(29, 13);
+            this.lblModifierOutilNom.TabIndex = 52;
+            this.lblModifierOutilNom.Text = "Nom";
+            // 
+            // lblModifierOutilIdCategorie
+            // 
+            this.lblModifierOutilIdCategorie.AutoSize = true;
+            this.lblModifierOutilIdCategorie.Location = new System.Drawing.Point(20, 83);
+            this.lblModifierOutilIdCategorie.Name = "lblModifierOutilIdCategorie";
+            this.lblModifierOutilIdCategorie.Size = new System.Drawing.Size(78, 13);
+            this.lblModifierOutilIdCategorie.TabIndex = 51;
+            this.lblModifierOutilIdCategorie.Text = "Id de categorie";
+            // 
+            // lblModifierOutilIdOutil
+            // 
+            this.lblModifierOutilIdOutil.AutoSize = true;
+            this.lblModifierOutilIdOutil.Location = new System.Drawing.Point(20, 45);
+            this.lblModifierOutilIdOutil.Name = "lblModifierOutilIdOutil";
+            this.lblModifierOutilIdOutil.Size = new System.Drawing.Size(38, 13);
+            this.lblModifierOutilIdOutil.TabIndex = 50;
+            this.lblModifierOutilIdOutil.Text = "Id outil";
+            // 
+            // ModifierOutilForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(270, 346);
+            this.Controls.Add(this.btnModifierOutilAnnuler);
+            this.Controls.Add(this.btnModifierOutilOk);
+            this.Controls.Add(this.txtModifierOutilNbLocations);
+            this.Controls.Add(this.txtModifierOutilDateInscription);
+            this.Controls.Add(this.txtModifierOutilEmail);
+            this.Controls.Add(this.txtModifierOutilTelephone);
+            this.Controls.Add(this.txtModifierOutilPrenom);
+            this.Controls.Add(this.txtModifierOutilNom);
+            this.Controls.Add(this.txtModifierOutilIdOutil);
+            this.Controls.Add(this.lblModifierOutilDescription);
+            this.Controls.Add(this.lblModifierOutilPrixLocation);
+            this.Controls.Add(this.lblModifierOutilDateAcquisition);
+            this.Controls.Add(this.lblModifierOutilPrenom);
+            this.Controls.Add(this.lblModifierOutilNom);
+            this.Controls.Add(this.lblModifierOutilIdCategorie);
+            this.Controls.Add(this.lblModifierOutilIdOutil);
+            this.Name = "ModifierOutilForm";
+            this.Text = "Modifier un outil";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnModifierOutilAnnuler;
+        private System.Windows.Forms.Button btnModifierOutilOk;
+        private System.Windows.Forms.TextBox txtModifierOutilNbLocations;
+        private System.Windows.Forms.TextBox txtModifierOutilDateInscription;
+        private System.Windows.Forms.TextBox txtModifierOutilEmail;
+        private System.Windows.Forms.TextBox txtModifierOutilTelephone;
+        private System.Windows.Forms.TextBox txtModifierOutilPrenom;
+        private System.Windows.Forms.TextBox txtModifierOutilNom;
+        private System.Windows.Forms.TextBox txtModifierOutilIdOutil;
+        private System.Windows.Forms.Label lblModifierOutilDescription;
+        private System.Windows.Forms.Label lblModifierOutilPrixLocation;
+        private System.Windows.Forms.Label lblModifierOutilDateAcquisition;
+        private System.Windows.Forms.Label lblModifierOutilPrenom;
+        private System.Windows.Forms.Label lblModifierOutilNom;
+        private System.Windows.Forms.Label lblModifierOutilIdCategorie;
+        private System.Windows.Forms.Label lblModifierOutilIdOutil;
+    }
+}
