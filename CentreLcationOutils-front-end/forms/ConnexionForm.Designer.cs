27d60c540ae5076378e95aa73f4fb4557a23ca31
David Gallego <davidgallego0125@gmail.com>
2014-12-09 20:23:31 -0500

    update connexion form et ajout de dossier images

--- CentreLcationOutils-front-end/forms/ConnexionForm.Designer.cs
+++ CentreLcationOutils-front-end/forms/ConnexionForm.Designer.cs
@@ -28,34 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AccueilForm));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.connexionControl1 = new ConnexionControl.ConnexionControl();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(265, 45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(129, 120);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // connexionControl1
             // 
-            this.connexionControl1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.connexionControl1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.connexionControl1.labelPassword = "Password :";
             this.connexionControl1.labelUserName = "Username :";
-            this.connexionControl1.Location = new System.Drawing.Point(37, 34);
+            this.connexionControl1.Location = new System.Drawing.Point(225, 253);
             this.connexionControl1.Name = "connexionControl1";
-            this.connexionControl1.Size = new System.Drawing.Size(218, 176);
-            this.connexionControl1.TabIndex = 0;
+            this.connexionControl1.Size = new System.Drawing.Size(201, 146);
+            this.connexionControl1.TabIndex = 1;
             // 
-            // ConnexionForm
+            // accueilForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(284, 222);
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(650, 453);
             this.Controls.Add(this.connexionControl1);
-            this.Name = "ConnexionForm";
-            this.Text = "Connectez-vous!";
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "accueilForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "OutilMate";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private System.Windows.Forms.PictureBox pictureBox1;
         private ConnexionControl.ConnexionControl connexionControl1;
     }
 }
