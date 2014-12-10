3b6ebc3fa84cb064cd50cc14c75ccbf9715d0b42
David Gallego <davidgallego0125@gmail.com>
2014-12-09 21:26:15 -0500

    ajout de accueilFormV2 et d'images

---
+++ CentreLcationOutils-front-end/forms/AccueilFormV2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CentreLcationOutils_front_end.forms
+{
+    public partial class AccueilFormV2 : Form
+    {
+        public AccueilFormV2()
+        {
+            InitializeComponent();
+        }
+    }
+}
