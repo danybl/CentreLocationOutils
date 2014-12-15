42f8069b9e8119d2f7190641944e66cdce112cb5
David Gallego <davidgallego0125@gmail.com>
2014-12-15 18:57:13 -0500

    commit

--- CentreLcationOutils-front-end/forms/AccueilForm.Designer.cs
+++ CentreLcationOutils-front-end/forms/AccueilForm.Designer.cs
@@ -39,6 +39,8 @@
             this.btnAccueil_GestionClients = new System.Windows.Forms.Button();
             this.btnAccueil_GestionLocations = new System.Windows.Forms.Button();
             this.lblAccueil_GestionFactures = new System.Windows.Forms.Label();
+            this.lblAccueil_GestionReservations = new System.Windows.Forms.Label();
+            this.btnAccueil_GestionReservations = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblAccueil_Titre
@@ -68,7 +70,7 @@
             // 
             this.lblAccueil_GestionClients.AutoSize = true;
             this.lblAccueil_GestionClients.Font = new System.Drawing.Font("Stencil", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblAccueil_GestionClients.Location = new System.Drawing.Point(205, 222);
+            this.lblAccueil_GestionClients.Location = new System.Drawing.Point(127, 222);
             this.lblAccueil_GestionClients.Name = "lblAccueil_GestionClients";
             this.lblAccueil_GestionClients.Size = new System.Drawing.Size(135, 16);
             this.lblAccueil_GestionClients.TabIndex = 3;
@@ -78,7 +80,7 @@
             // 
             this.lblAccueil_GestionOutils.AutoSize = true;
             this.lblAccueil_GestionOutils.Font = new System.Drawing.Font("Stencil", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblAccueil_GestionOutils.Location = new System.Drawing.Point(480, 222);
+            this.lblAccueil_GestionOutils.Location = new System.Drawing.Point(555, 222);
             this.lblAccueil_GestionOutils.Name = "lblAccueil_GestionOutils";
             this.lblAccueil_GestionOutils.Size = new System.Drawing.Size(119, 16);
             this.lblAccueil_GestionOutils.TabIndex = 5;
@@ -88,7 +90,7 @@
             // 
             this.lblAccueil_GestionLocations.AutoSize = true;
             this.lblAccueil_GestionLocations.Font = new System.Drawing.Font("Stencil", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblAccueil_GestionLocations.Location = new System.Drawing.Point(456, 386);
+            this.lblAccueil_GestionLocations.Location = new System.Drawing.Point(532, 386);
             this.lblAccueil_GestionLocations.Name = "lblAccueil_GestionLocations";
             this.lblAccueil_GestionLocations.Size = new System.Drawing.Size(158, 16);
             this.lblAccueil_GestionLocations.TabIndex = 7;
@@ -98,7 +100,7 @@
             // 
             this.btnAccueil_GestionFactures.BackColor = System.Drawing.Color.Tan;
             this.btnAccueil_GestionFactures.Image = ((System.Drawing.Image)(resources.GetObject("btnAccueil_GestionFactures.Image")));
-            this.btnAccueil_GestionFactures.Location = new System.Drawing.Point(196, 407);
+            this.btnAccueil_GestionFactures.Location = new System.Drawing.Point(119, 407);
             this.btnAccueil_GestionFactures.Name = "btnAccueil_GestionFactures";
             this.btnAccueil_GestionFactures.Size = new System.Drawing.Size(155, 142);
             this.btnAccueil_GestionFactures.TabIndex = 6;
@@ -108,7 +110,7 @@
             // 
             this.btnAccueil_GestionOutils.BackColor = System.Drawing.Color.Tan;
             this.btnAccueil_GestionOutils.Image = ((System.Drawing.Image)(resources.GetObject("btnAccueil_GestionOutils.Image")));
-            this.btnAccueil_GestionOutils.Location = new System.Drawing.Point(459, 241);
+            this.btnAccueil_GestionOutils.Location = new System.Drawing.Point(535, 241);
             this.btnAccueil_GestionOutils.Name = "btnAccueil_GestionOutils";
             this.btnAccueil_GestionOutils.Size = new System.Drawing.Size(155, 142);
             this.btnAccueil_GestionOutils.TabIndex = 4;
@@ -118,7 +120,7 @@
             // 
             this.btnAccueil_GestionClients.BackColor = System.Drawing.Color.Tan;
             this.btnAccueil_GestionClients.Image = ((System.Drawing.Image)(resources.GetObject("btnAccueil_GestionClients.Image")));
-            this.btnAccueil_GestionClients.Location = new System.Drawing.Point(196, 241);
+            this.btnAccueil_GestionClients.Location = new System.Drawing.Point(119, 241);
             this.btnAccueil_GestionClients.Name = "btnAccueil_GestionClients";
             this.btnAccueil_GestionClients.Size = new System.Drawing.Size(155, 142);
             this.btnAccueil_GestionClients.TabIndex = 2;
@@ -129,7 +131,7 @@
             // 
             this.btnAccueil_GestionLocations.BackColor = System.Drawing.Color.Tan;
             this.btnAccueil_GestionLocations.Image = ((System.Drawing.Image)(resources.GetObject("btnAccueil_GestionLocations.Image")));
-            this.btnAccueil_GestionLocations.Location = new System.Drawing.Point(459, 405);
+            this.btnAccueil_GestionLocations.Location = new System.Drawing.Point(535, 407);
             this.btnAccueil_GestionLocations.Name = "btnAccueil_GestionLocations";
             this.btnAccueil_GestionLocations.Size = new System.Drawing.Size(155, 142);
             this.btnAccueil_GestionLocations.TabIndex = 8;
@@ -139,11 +141,31 @@
             // 
             this.lblAccueil_GestionFactures.AutoSize = true;
             this.lblAccueil_GestionFactures.Font = new System.Drawing.Font("Stencil", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblAccueil_GestionFactures.Location = new System.Drawing.Point(193, 386);
+            this.lblAccueil_GestionFactures.Location = new System.Drawing.Point(120, 386);
             this.lblAccueil_GestionFactures.Name = "lblAccueil_GestionFactures";
             this.lblAccueil_GestionFactures.Size = new System.Drawing.Size(154, 16);
             this.lblAccueil_GestionFactures.TabIndex = 9;
             this.lblAccueil_GestionFactures.Text = "Gestion des Factures";
+            // 
+            // lblAccueil_GestionReservations
+            // 
+            this.lblAccueil_GestionReservations.AutoSize = true;
+            this.lblAccueil_GestionReservations.Font = new System.Drawing.Font("Stencil", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAccueil_GestionReservations.Location = new System.Drawing.Point(316, 303);
+            this.lblAccueil_GestionReservations.Name = "lblAccueil_GestionReservations";
+            this.lblAccueil_GestionReservations.Size = new System.Drawing.Size(177, 16);
+            this.lblAccueil_GestionReservations.TabIndex = 10;
+            this.lblAccueil_GestionReservations.Text = "Gestion de reservations";
+            // 
+            // btnAccueil_GestionReservations
+            // 
+            this.btnAccueil_GestionReservations.BackColor = System.Drawing.Color.Tan;
+            this.btnAccueil_GestionReservations.Image = ((System.Drawing.Image)(resources.GetObject("btnAccueil_GestionReservations.Image")));
+            this.btnAccueil_GestionReservations.Location = new System.Drawing.Point(329, 322);
+            this.btnAccueil_GestionReservations.Name = "btnAccueil_GestionReservations";
+            this.btnAccueil_GestionReservations.Size = new System.Drawing.Size(155, 142);
+            this.btnAccueil_GestionReservations.TabIndex = 11;
+            this.btnAccueil_GestionReservations.UseVisualStyleBackColor = false;
             // 
             // AccueilForm
             // 
@@ -151,6 +173,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SteelBlue;
             this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.btnAccueil_GestionReservations);
+            this.Controls.Add(this.lblAccueil_GestionReservations);
             this.Controls.Add(this.lblAccueil_GestionFactures);
             this.Controls.Add(this.btnAccueil_GestionLocations);
             this.Controls.Add(this.lblAccueil_GestionLocations);
@@ -181,5 +205,7 @@
         private System.Windows.Forms.Label lblAccueil_GestionLocations;
         private System.Windows.Forms.Button btnAccueil_GestionLocations;
         private System.Windows.Forms.Label lblAccueil_GestionFactures;
+        private System.Windows.Forms.Label lblAccueil_GestionReservations;
+        private System.Windows.Forms.Button btnAccueil_GestionReservations;
     }
 }
